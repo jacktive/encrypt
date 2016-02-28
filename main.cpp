9977bcc0a492405a278f1af61cf08d9ae81870ec
minary <min050820@gmail.com>
2016-02-28 12:26:01 +0900

    update parameter
    File I/O is still broken.
    Now it accepts parameter like "-edc" (same as  "-e -d -c")

--- main.cpp
+++ main.cpp
@@ -1,7 +1,58 @@
 #include <iostream>
+#include <string>
 #include <cstring>
 #include <cstdio>
-#include <string>
+#include <unistd.h>
+
+#ifdef __WIN32
+#include <conio.h>
+#include <Windows.h>
+#endif // __WIN32
+#ifdef __linux
+#include <termios.h>
+int getch(void) {
+    struct termios oldattr, newattr;
+    int ch;
+    tcgetattr( STDIN_FILENO, &oldattr );
+    newattr = oldattr;
+    newattr.c_lflag &= ~( ICANON | ECHO );
+    tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
+    ch = getchar();
+    tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
+    return ch;
+}
+int getche(void) {
+    struct termios oldattr, newattr;
+    int ch;
+    tcgetattr( STDIN_FILENO, &oldattr );
+    newattr = oldattr;
+    newattr.c_lflag &= ~( ICANON );
+    tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
+    ch = getchar();
+    tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
+    return ch;
+}
+bool kbhit(void) {
+  struct termios oldt, newt;
+  int ch;
+  int oldf;
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+  ch = getchar();
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  fcntl(STDIN_FILENO, F_SETFL, oldf);
+  if(ch != EOF)
+  {
+    ungetc(ch, stdin);
+    return true;
+  }
+  return false;
+}
+#endif
 
 #include "sha256.h"
 #include "endetool.h"
@@ -9,17 +60,19 @@
 using namespace std;
 
 const char str_help[][128]={
-    "Usage: encrypt [options] [<key> <data>]",
+    "Usage: ",
+    " [options] [<key> <data>]",
     "<key>=key to be used in encryption/decryption.",
     "<data>=data to be encrypted/decrypted.",
     "Options:",
     "\t-e (--encrypt) to encrypt a data",
     "\t-d (--decrypt) to decrypt a data",
-    "\t\tNote that if encrypt and decrypt are used at the same time, it'll essentially encrypt.",
-    "\t-f (--write-to-file) to write result to the file.",
-    "\t-F (--read-from-file) to read <key> and <data> from file.",
+    "\t\tNote that if encrypt and decrypt are used at the same time, it'll basically encrypt and print original.",
+    "\t<BROKEN> -w (--write-to-file) to write result to the file.",
+    "\t<BROKEN> -r (--read-from-file) to read <key> and <data> from file.",
     "\t\tThese are three files that could be used. 'encrypted.txt','decrypted.txt','key.txt'",
     "\t\tNon-exiting files will be considered empty.",
+    "\t-D (--dialog) Launch this program in dialog mode.",
     "\t-B (--no-blocking) to keep this program from blocking",
     "\t-h (--help) for this help page.",
     "You may also call this program with no parameter to launch in dialog mode.",
@@ -46,29 +99,54 @@ const char str_dialog[][128]={
 
 string decrypted, encrypted;
 string key_str;
+string prgm_name;
+
 namespace flags {
     bool dialogMode;
     bool encrypt;
     bool decrypt;
     bool writeToFile;
     bool readFromFile;
-    bool blocking=true;
+    bool noBlocking;
     bool helpPage;
 }
 
 void help(bool tab=false) {
-    for(int i=0; i<(sizeof(str_help)/128); i++)
+    cout << (!tab?'\0':'\t') << str_help[0] << prgm_name << str_help[1] << endl;
+    for(int i=2; i<(sizeof(str_help)/128); i++)
         cout << (!tab?'\0':'\t') << str_help[i] << endl;
 }
 
-#define d_pause if(flags::blocking)getchar()
+void d_pause() {
+    const char paused[]="Press 'A' to continue";
+    static int blink=0;
+    while(!kbhit()) {
+        for(int i=0; i<sizeof(paused); i++) {
+            if(i!=blink)
+                putchar(paused[i]);
+            else
+                putchar(' ');
+            if(blink>=sizeof(paused)-1)
+                blink=0;
+        }
+        putchar('\r');
+        blink++;
+        Sleep(66);
+    }
+    getch();
+    putchar('\r');
+    for(int i=0; i<sizeof(paused); i++)
+        putchar(' ');
+    putchar('\r');
+}
+
 void dialog() {
     int select=0;
     cout << str_dialog[4];
-    d_pause;
+    d_pause();
     help(true);
     cout << str_dialog[5];
-    d_pause;
+    d_pause();
 again:
     cout << str_dialog[6] << str_dialog[0];
     cin >> select;
@@ -82,32 +160,90 @@ again:
     if(select==2 || select==3)
         flags::decrypt=true;
     cout << str_dialog[8] << str_dialog[select] << '!' << '\n';
-    d_pause;
+    d_pause();
     cout << str_dialog[9] << str_dialog[select] << '?' << '\n' << str_dialog[0];
     cin >> encrypted;
     decrypted=encrypted;
     cout << str_dialog[10] << str_dialog[select] << str_dialog[11];
-    d_pause;
+    d_pause();
     cout << str_dialog[12] << str_dialog[0];
     cin >> key_str;
     cout << str_dialog[13];
-    d_pause;
+    d_pause();
     cout << str_dialog[14];
-    d_pause;
+    d_pause();
 }
 
 void pharse(int argc, char **argv) {
-    for(int i=1; i<argc; i++) {
-        if(strcmp(argv[i], "-e")==0 || strcmp(argv[i], "--encrypt")==0)
+    for(int i=1; i<argc-2; i++) {
+        if(argv[i][0]=='-' && argv[i][1]!='-') {
+            for(int j=0; argv[i][j]!=0; j++) {
+                switch(argv[i][j]) {
+                case 'e':
+                    flags::encrypt=true;
+                    break;
+                case 'd':
+                    flags::decrypt=true;
+                    break;
+                case 'w':
+                    flags::writeToFile=true;
+                    break;
+                case 'r':
+                    flags::readFromFile=true;
+                    break;
+                case 'D':
+                    flags::dialogMode=true;
+                    break;
+                case 'B':
+                    flags::noBlocking=true;
+                    break;
+                default:
+                    flags::helpPage=true;
+                }
+            }
+        }
+        else if(strcmp(argv[i], "--encrypt")==0)
             flags::encrypt=true;
-        else if(strcmp(argv[i], "-d")==0 || strcmp(argv[i], "--decrypt")==0)
+        else if(strcmp(argv[i], "--decrypt")==0)
             flags::decrypt=true;
-        else if(strcmp(argv[i], "-f")==0 || strcmp(argv[i], "--write-to-file")==0)
+        else if(strcmp(argv[i], "--write-to-file")==0)
             flags::writeToFile=true;
-        else if(strcmp(argv[i], "-F")==0 || strcmp(argv[i], "--read-from-file")==0)
+        else if(strcmp(argv[i], "--read-from-file")==0)
             flags::readFromFile=true;
-        else if(strcmp(argv[i], "-B")==0 || strcmp(argv[i], "--no-blocking")==0)
-            flags::blocking=false;
+        else if(strcmp(argv[i], "-dialog")==0)
+            flags::dialogMode=true;
+        else if(strcmp(argv[i], "--no-blocking")==0)
+            flags::noBlocking=true;
+        else
+            flags::helpPage=true;
+    }
+    if(flags::readFromFile) {
+        if(strcmp(argv[0], "--encrypt")==0)
+            flags::encrypt=true;
+        else if(strcmp(argv[0], "--decrypt")==0)
+            flags::decrypt=true;
+        else if(strcmp(argv[0], "--write-to-file")==0)
+            flags::writeToFile=true;
+        else if(strcmp(argv[0], "--read-from-file")==0)
+            flags::readFromFile=true;
+        else if(strcmp(argv[0], "-dialog")==0)
+            flags::dialogMode=true;
+        else if(strcmp(argv[0], "--no-blocking")==0)
+            flags::noBlocking=true;
+        else
+            flags::helpPage=true;
+        if(strcmp(argv[1], "--encrypt")==0)
+            flags::encrypt=true;
+        else if(strcmp(argv[1], "--decrypt")==0)
+            flags::decrypt=true;
+        else if(strcmp(argv[1], "--write-to-file")==0)
+            flags::writeToFile=true;
+        else if(strcmp(argv[1], "--read-from-file")==0)
+            flags::readFromFile=true;
+        else if(strcmp(argv[1], "-dialog")==0)
+            flags::dialogMode=true;
+        else if(strcmp(argv[1], "--no-blocking")==0)
+            flags::noBlocking=true;
         else
             flags::helpPage=true;
     }
@@ -169,10 +305,14 @@ void encrypt() {
     endetool.cryptkey(sha256.getHash().c_str());
     endetool.text(decrypted.c_str());
     encrypted=endetool.encodedtext();
+    encrypted.erase(encrypted.end()-1);
+    encrypted.erase(encrypted.end()-1);
     cout << "Encrypted: " << encrypted << endl;
 }
 
 void decrypt() {
+    encrypted.push_back('=');
+    encrypted.push_back('=');
     EnDeTool endetool;
     SHA256 sha256;
     sha256(key_str);
@@ -183,15 +323,22 @@ void decrypt() {
 }
 
 int main(int argc, char **argv) {
-    if(argc==1) {
+    cout << "Loading...";
+    encrypted="";
+    decrypted="";
+    key_str="";
+    #ifdef __WIN32
+    prgm_name="encrypt.exe";
+    #else
+    prgm_name="encrypt";
+    #endif
+    if(argc==1)
         flags::dialogMode=true;
-        dialog();
-    }
     else {
         pharse(argc, argv);
         if(flags::helpPage) {
             help();
-            return 1;
+            return 0;
         }
         if(!flags::readFromFile) {
             key_str=argv[argc-2];
@@ -199,9 +346,16 @@ int main(int argc, char **argv) {
             decrypted=argv[argc-1];
         }
     }
+    cout << '\r' << "          " << '\r';
+    if(flags::dialogMode)
+        dialog();
     if(flags::encrypt)
         encrypt();
     if(flags::decrypt)
         decrypt();
+    if(flags::writeToFile)
+        fileOperation(true);
+    if(!flags::noBlocking)
+        d_pause();
     return 0;
 }
